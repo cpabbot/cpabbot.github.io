94b077d62c03f8560be719d9ca0a32c944eb64d6
cpabbot <cpabbot1@gmail.com>
2017-08-19 23:29:36 -0400

    added Programs page and two programs

---
+++ js/window.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    
+    var url = "";
+    
+    // OPEN WINDOW
+    $('.card__title').click(function() {
+        $('.blur-background').removeClass('hide');
+        $('.window').removeClass('hide');
+        
+        url = $(this).attr('id');
+        $('.window__frame').attr('src', url + ".html")
+    });
+    
+    // CLOSE WINDOW
+    $('.blur-background').click(function() {
+        $('.window').addClass('hide');
+        $('.blur-background').addClass('hide');
+    });
+    
+});
