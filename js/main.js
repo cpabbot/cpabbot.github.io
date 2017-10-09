bb90ea92a6974b9ab5f1f4316ae32d1edf2202c7
Cameron Abbot <cpabbot1@gmail.com>
2017-10-08 22:52:37 -0400

    projects page: created format example

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
 $(document).ready(function() {
     
     $(window).scroll(function () {
-        var offset = $('.about').offset().top;
-        var wHeight = $(window).height();
-        var below = 200; // custom amount between top of object and bottom of viewport that object should animate
+        //var offset = $('.about').offset().top;
+        //var wHeight = $(window).height();
+        //var below = 200; // custom amount between top of object and bottom of viewport that object should animate
         
-        /*alert("top: " + $(window).scrollTop() + " offset: " + offset + " wHeight: " + 
-wHeight);*/
+        //alert("top: " + $(window).scrollTop() + " offset: " + offset + " wHeight: " + wHeight);
         
-        if($(window).scrollTop() > offset - wHeight + below) {
+        /*if($(window).scrollTop() > offset - wHeight + below) {
             $('.about__img').addClass("fadeDown");
         }
         else {
         //element.removeClass("animateMe");
+        }*/
+        
+        var gradientOffset = $('.intro2').offset().top;
+        // Fade in main nav buttons (bottom of home page)
+        if($(window).scrollTop() > gradientOffset - 100) {
+            $('.main-nav-btn').addClass("fadeUp");
         }
+        else {
+            //alert($(window).scrollTop());
+        }
+        
+        
     });
     
     
