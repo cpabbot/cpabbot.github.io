dfd92e0b65bf1446673f757e3176a4ee1f1116d8
Cameron Abbot <cpabbot1@gmail.com>
2018-06-27 00:04:32 -0400

    added about description message, basic structure for photo section

--- js/main.js
+++ js/main.js
@@ -14,9 +14,13 @@ $(document).ready(function() {
         //element.removeClass("animateMe");
         }*/
         
-        var gradientOffset = $('.intro2').offset().top;
-        // Fade in main nav buttons (bottom of home page)
-        if($(window).scrollTop() > gradientOffset - 100) {
+        var gradientOffset = $('.about--1').offset().top;
+        
+        if($(window).scrollTop() > gradientOffset) {
+            $(".e--1").addClass("fix");
+        }
+        if($(window).scrollTop() > gradientOffset + 100) {
+            // Fade in main nav buttons (bottom of home page)
             $('.main-nav-btn').addClass("fadeUp");
         }
         else {
